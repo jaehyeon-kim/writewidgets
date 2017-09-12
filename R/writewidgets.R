d723a449a353e9f6519770d53d6f2ec98c03f0a6
Bernie Kim <jakim@ad.corelogic.asia>
2017-09-13 08:46:23 +1000

    meta updates

---
+++ R/writewidgets.R
@@ -0,0 +1,8 @@
+#' Write htmlwidgets without saving
+#'
+#' This package aims to extend \code{saveWidget} of the htmlwidgets package.
+#'
+#' @name writewidgets
+#' @import htmltools
+#' @import htmlwidgets
+NULL
