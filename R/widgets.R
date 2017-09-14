3112947486676dc27cbe2def4b12db08d24c03a7
Jaehyeon Kim <Jaehyeon.Kim@corelogic.com.au>
2017-09-14 07:40:48 +0000

    write_widget() up'

--- R/widgets.R
+++ R/widgets.R
@@ -1,11 +1,29 @@
-write_widget <- function(widget, from = '/usr/local/lib/R/site-library/', to = NULL, background = 'white', to_save = FALSE, to_raw = TRUE) {
+update_dep_path <- function(dep, libdir = 'lib') {
+    dir <- dep$src$file
+    if (!is.null(dep$package))
+        dir <- system.file(dir, package = dep$package)
+
+    if (length(libdir) != 1 || libdir %in% c("", "/"))
+        stop("libdir must be of length 1 and cannot be '' or '/'")
+
+    target <- if (getOption('htmltools.dir.version', TRUE)) {
+        paste(dep$name, dep$version, sep = '-')
+    } else {
+        dep$name
+    }
+    dep$src$file <- file.path(libdir, target)
+    dep
+}
+
+write_widget <- function(widget, libdir = 'lib', cdn = NULL, background = 'white', to_save = FALSE, to_raw = FALSE) {
     html <- htmlwidgets:::toHTML(widget, standalone = TRUE, knitrOptions = list())
 
     rendered <- htmltools::renderTags(html)
     deps <- rendered$dependencies
-    deps_rendered <- htmltools::renderDependencies(dependencies = deps, srcType = c('hred', 'file'))
-    deps_updated <- if (!is.null(to)) {
-        gsub(from, to, deps_rendered)
+    deps_up <- lapply(deps, update_dep_path, libdir = libdir)
+    deps_rendered <- htmltools::renderDependencies(dependencies = deps_up, srcType = c('hred', 'file'))
+    deps_updated <- if (!is.null(cdn)) {
+        gsub(libdir, cdn, deps_rendered)
     } else {
         deps_rendered
     }
@@ -18,7 +36,7 @@ write_widget <- function(widget, from = '/usr/local/lib/R/site-library/', to = N
                 deps_updated,
                 rendered$head,
             "</head>",
-            sprintf("<body style='background-color:%s;'>", htmltools::htmlEscape(background)),
+                sprintf("<body style='background-color:%s;'>", htmltools::htmlEscape(background)),
                 rendered$html,
             "</body>",
         "</html>"
